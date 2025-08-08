d56667889142978f12165bfb0f3b31f4709883ac
Fuad Aliah <eye.design17@yahoo.com>
2025-08-08 15:46:17 +0300

    fix auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,48 +1,12 @@
-// lib/auth.ts
 import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
 import type { AuthOptions } from "next-auth";
 
 export const authOptions: AuthOptions = {
   providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const { username, password } = credentials as {
-          username: string;
-          password: string;
-        };
-
-        if (username === "admin" && password === "admin") {
-          return { id: "1", name: "Admin User", email: "admin@example.com" };
-        }
-
-        return null;
-      },
-    }),
-
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
-  pages: {
-    signIn: "/login",
-    error: "/login",
-  },
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      else if (new URL(url).origin === baseUrl) return url;
-      return baseUrl;
-    },
-  },
   secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: "jwt",
-  },
 };
