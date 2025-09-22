e35211091b1f5468e2960c78ccfa6540b7fab60d
Fuad Aliah <eye.design17@yahoo.com>
2025-09-23 00:09:28 +0300

    add PUT for users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,25 +1,45 @@
 import prisma from '@/lib/prisma';
-import { Prisma } from '@prisma/client';
+import { Language, Prisma } from '@prisma/client';
 import { hash } from 'bcryptjs';
 import { NextResponse } from 'next/server';
 import z from 'zod';
 
-const paramSchema = z.object({
+const querySchema = z.object({
     firstName: z.string().optional(),
     lastName: z.string().optional(),
     email: z.string().optional(),
-    role: z.string().optional(),
+    image: z.string().optional(),
     mobileNumber: z.string().optional(),
     defaultLanguage: z.string().optional(),
-    tempPassword: z.string().optional(),
     isActive: z.string().optional(),
+
+    // pagination + sorting
     currentPage: z.coerce.number().int().min(1).default(1),
     pageSize: z.coerce.number().int().min(1).max(100).default(10),
     sort: z.string().optional().default('createdAt'),
     order: z.enum(['asc', 'desc']).default('desc'),
-    createdAt: z.string().optional(),
 });
-function buildWhere(params: z.infer<typeof paramSchema>): Prisma.UserWhereInput {
+
+const createUserSchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.email(),
+    image: z.string().optional(),
+    mobileNumber: z.string().optional(),
+    defaultLanguage: z.enum(Language).default(Language.EN),
+    tempPassword: z.string().min(6),
+});
+
+const updateUserSchema = z.object({
+    firstName: z.string().optional(),
+    lastName: z.string().optional(),
+    mobileNumber: z.string().optional(),
+    defaultLanguage: z.enum(Language).optional(),
+    image: z.string().optional(),
+    isActive: z.boolean().optional(),
+});
+
+function buildWhere(params: z.infer<typeof querySchema>): Prisma.UserWhereInput {
     const { firstName } = params;
 
     const where: Prisma.UserWhereInput = {
@@ -32,7 +52,7 @@ function buildWhere(params: z.infer<typeof paramSchema>): Prisma.UserWhereInput 
 export async function GET(req: Request) {
     try {
         const { searchParams } = new URL(req.url);
-        const params = paramSchema.parse(Object.fromEntries(searchParams));
+        const params = querySchema.parse(Object.fromEntries(searchParams));
         const { currentPage, pageSize, sort, order } = params;
 
         const allowedSort = ['createdAt', 'email', 'firstName', 'lastName'];
@@ -65,7 +85,7 @@ export async function GET(req: Request) {
 export async function POST(req: Request) {
     try {
         const body = await req.json();
-        const data = paramSchema.parse(body);
+        const data = createUserSchema.parse(body);
         const { firstName, lastName, email, mobileNumber, tempPassword } = data;
 
         if (!email || !tempPassword) {
@@ -91,6 +111,36 @@ export async function POST(req: Request) {
         return NextResponse.json({ message: 'User created successfully', user }, { status: 201 });
     } catch (error) {
         return NextResponse.json({ error: `Internal Server Error ${error}` }, { status: 500 });
+    }
+}
+
+export async function PUT(req: Request) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get('id');
+
+        if (!id) {
+            return NextResponse.json({ error: 'User ID is required' }, { status: 400 });
+        }
+
+        const body = await req.json();
+        const data = updateUserSchema.parse(body);
+
+        const user = await prisma.user.update({
+            where: { id: id },
+            data: {
+                firstName: data.firstName,
+                lastName: data.lastName,
+                mobileNumber: data.mobileNumber,
+                defaultLanguage: (data.defaultLanguage as Language) || Language.EN,
+                image: data.image,
+                isActive: data.isActive,
+            },
+        });
+
+        return NextResponse.json({ user, message: 'User updated successfully' }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: `Failed to update user ${error}` }, { status: 500 });
     }
 }
 
