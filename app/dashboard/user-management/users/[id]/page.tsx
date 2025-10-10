babe6669ecbc72de4cff889549108ef834909cf6
Fuad Aliah <eye.design17@yahoo.com>
2025-10-10 16:37:29 +0300

    fixed for vercel

--- app/dashboard/user-management/users/[id]/page.tsx
+++ app/dashboard/user-management/users/[id]/page.tsx
@@ -1,19 +1,35 @@
-import prisma from '@/lib/prisma';
+'use client';
 
-export default async function UserPage({ params }: { params: { id: string } }) {
-    const { id } = params;
-    const user = await prisma.user.findUnique({
-        where: { id },
-    });
+import { useEffect, useState } from 'react';
 
-    if (!user) {
-        throw new Error('User not found');
-    }
+export default function UserPage({ params }: { params: { id: string } }) {
+    const [user, setUser] = useState('');
+    console.log('id', params.id);
+
+    const fetchUser = async () => {
+        try {
+            const query = new URLSearchParams({
+                id: params.id.toString(),
+            });
+
+            const res = await fetch(`/api/users?${query.toString()}`);
+
+            const data = await res.json();
+            setUser(data.data || []);
+        } catch (err) {
+            console.error('Error fetching user:', err);
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [params.id]);
 
     return (
         <div>
-            <h1>{user.firstName}</h1>
-            <p>{user.email}</p>
+            <h1>{JSON.stringify(user)}</h1>
+            {/* <p>{user.email}</p> */}
         </div>
     );
 }
